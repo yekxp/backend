327313a20c0d24b11a361e7be704c4c99c4f8d52
mert18 <mertuygur02@gmail.com>
2023-07-22 12:56:00 +0300

    1: Spring boot project initialized.

---
+++ src/test/java/com/developers/sosyalapp/SosyalappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developers.sosyalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosyalappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
