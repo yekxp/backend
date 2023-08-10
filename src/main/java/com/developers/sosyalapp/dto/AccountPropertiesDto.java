2be7e25f3eafd9723ee29f7faec84a31f8660121
mert18 <mertuygur02@gmail.com>
2023-08-10 19:04:17 +0300

    31: account properties dto oluşturuldu.

---
+++ src/main/java/com/developers/sosyalapp/dto/AccountPropertiesDto.java
@@ -0,0 +1,58 @@
+package com.developers.sosyalapp.dto;
+
+public class AccountPropertiesDto {
+    private String accountId;
+    private int age;
+    private boolean smoking;
+    private double income;
+    private String city;
+    private String district;
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isSmoking() {
+        return smoking;
+    }
+
+    public void setSmoking(boolean smoking) {
+        this.smoking = smoking;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+}
