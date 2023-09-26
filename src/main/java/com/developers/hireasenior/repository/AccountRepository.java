cd2ce11aee0a6bae58f367a0067eb3c3da2f76f4
mert18 <mertuygur02@gmail.com>
2023-09-26 22:11:15 +0300

    121: login message corrected when failed login.

--- src/main/java/com/developers/hireasenior/repository/AccountRepository.java
+++ src/main/java/com/developers/hireasenior/repository/AccountRepository.java
@@ -3,7 +3,9 @@ package com.developers.hireasenior.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.developers.hireasenior.model.Account;
 
+import java.util.Optional;
+
 
 public interface AccountRepository extends JpaRepository<Account, String>  {
-    Account findByEmail(String email);
+    Optional<Account> findByEmail(String email);
 }
